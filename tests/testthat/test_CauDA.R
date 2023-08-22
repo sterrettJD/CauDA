cf0c04f9d4510796b3f910fd486c4b2ed2889029
John Sterrett <sterrettJD@protonmail.com>
2023-08-22 13:31:18 -0600

    add testing func for confounded dataset

--- tests/testthat/test_CauDA.R
+++ tests/testthat/test_CauDA.R
@@ -1,10 +1,11 @@
 make_simple_confounded_dataset <- function(nsamples=1000){
+  set.seed(42)
   # confounder
   tax.c <- rnbinom(n=nsamples, size=500, prob=0.3)
   # taxon X (independent var)
   tax.x <- 0.5*tax.c + 0.5*rnbinom(n=nsamples, size=500, prob=0.3)
   # Y (dependent var)
-  y <- 0.5*tax.c + 0.5*rnorm(nsamples, mean=mean(tax.c), sd=sd(tax.c))
+  y <- 0.5*tax.x + 0.4*tax.c + 0.1*rnorm(nsamples, mean=mean(tax.c), sd=sd(tax.c))
   # edge matrix to show that tax.c has a causal effect on tax.x
   edge.dictionary <- matrix(c(0, 0,
                               1, 0),
@@ -14,7 +15,7 @@ make_simple_confounded_dataset <- function(nsamples=1000){
 
   new("CauDAbox",
       metadata=matrix(data=y, nrow=nsamples),
-      tableX=matrix(data=c(tax.c, tax.x), nrow=nsamples),
+      tableX=matrix(data=c(tax.x, tax.c), nrow=nsamples),
       dictionary=edge.dictionary
       )
 }
@@ -34,5 +35,21 @@ testthat::test_that("create dataset", {
   expect_s4_class(x, "CauDAbox")
 }
 )
+
+testthat::test_that("taxa have correct coef", {
+  data <- make_simple_confounded_dataset(1000)
+
+  y <- data@metadata[,1]
+  tax.x <- data@tableX[,1]
+  tax.c <- data@tableX[,2]
+  mod <- lm(y ~ tax.x + tax.c)
+
+  expect_equal(coef(mod)[2:3],
+               c(0.5, 0.4),
+               tolerance=0.05,
+               ignore_attr = TRUE)
+}
+)
+
 
 
