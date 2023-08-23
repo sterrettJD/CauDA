43903a3b8885c741c6eda8055d0dae4667629596
John Sterrett <sterrettJD@protonmail.com>
2023-08-23 10:55:38 -0600

    add tests for making formula and regressing

--- tests/testthat/test_CauDA.R
+++ tests/testthat/test_CauDA.R
@@ -7,13 +7,18 @@ make_simple_confounded_dataset <- function(nsamples=1000,
   tax.c <- rnbinom(n=nsamples, size=500, prob=0.3)
   # taxon X (independent var)
   tax.x <- c_on_x*tax.c + (1-c_on_x)*rnbinom(n=nsamples, size=500, prob=0.3)
+  # matrix of taxa
+  count.table <- matrix(data=c(tax.x, tax.c), nrow=nsamples)
+  colnames(count.table) <- c("tax.x", "tax.c")
+
   # Y (dependent var)
   y <- x_on_y*tax.x +
        c_on_y*tax.c +
        (1-x_on_y-c_on_y)*rnorm(nsamples, mean=mean(tax.c), sd=sd(tax.c))
+
   # edge matrix to show that tax.c has a causal effect on tax.x
-  edge.dictionary <- matrix(c(0, 0,
-                              1, 0),
+  edge.dictionary <- matrix(c(0,         0,
+                              c_on_x!=0, 0),
                             nrow=2,
                             byrow=T)
   rownames(edge.dictionary) <- c("tax.x", "tax.c")
@@ -21,7 +26,7 @@ make_simple_confounded_dataset <- function(nsamples=1000,
 
   new("CauDAbox",
       metadata=matrix(data=y, nrow=nsamples),
-      tableX=matrix(data=c(tax.x, tax.c), nrow=nsamples),
+      tableX=count.table,
       dictionary=edge.dictionary
       )
 }
@@ -85,5 +90,38 @@ testthat::test_that("confounded taxon is confounded", {
 }
 )
 
+testthat::test_that("RHS_from_edgemat gets the right stuff", {
+  c.on.x <- 0.5
+  x.on.y <- 0.5
+  c.on.y <- 0.4
+  data <- make_simple_confounded_dataset(1000,
+                                         c_on_x=c.on.x,
+                                         x_on_y=x.on.y,
+                                         c_on_y=c.on.y)
 
+  expect_equal(RHS_from_edgemat(data, "tax.x"),
+               "tax.x + tax.c")
+}
+)
+
+testthat::test_that("RHS_from_edgemat gets the right model output", {
+  c.on.x <- 0.5
+  x.on.y <- 0.5
+  c.on.y <- 0.4
+  data <- make_simple_confounded_dataset(1000,
+                                         c_on_x=c.on.x,
+                                         x_on_y=x.on.y,
+                                         c_on_y=c.on.y)
+
+  rhs <- RHS_from_edgemat(data, "tax.x")
+  y <- data@metadata[,1]
+  mod <- lm(as.formula(paste0("y ~ ", rhs)),
+            data=make_model_df(data, "tax.x"))
+
+  expect_equal(coef(mod)[2:3],
+               c(x.on.y, c.on.y),
+               tolerance=0.05,
+               ignore_attr = TRUE)
+}
+)
 
